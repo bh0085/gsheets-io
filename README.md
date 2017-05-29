ae44b1fe6aa094cde04491fb70db5f619bf1b895
Ubuntu <ubuntu@ip-172-31-10-183.ec2.internal>
2017-05-29 18:24:13 +0000

    adds type checking and install with setup toools

--- README.md
+++ README.md
@@ -3,4 +3,15 @@
 
 
 default share email (used by ben):
-untappd-python-reporting@polished-bounty-162720.iam.gserviceaccount.com 
+
+# api key username
+<pre><code>untappd-python-reporting@polished-bounty-162720.iam.gserviceaccount.com</pre></code>
+
+
+# sample commands
+
+### occupancy reports
+./get-google-sheet.py --sqlite --sheet="1EOzXwcts0nBFjQIMMfK6i6D4jk_AIbtNHCFvjmUfCF0" --name="DATABASE" --header-rows=4 --types-row=2 --index-row=3 --table=linedata --output=occupancy.db
+
+### shows output
+./get-google-sheet.py --sqlite --sheet="1WWOD92D7SvCVh2Hlu-0dyyuIOctn7UMVMSOPoFBbGgI" --name="DATABASE" --header-rows=4 --types-row=2 --index-row=3 --table=bands --output=bands.db
--- get-google-sheet.py
+++ get-google-sheet.py
@@ -4,6 +4,8 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import calendar, json, re, os
 import sqlite3
+import sqlalchemy
+import dateparser
 
 
 #connects to google spreadsheets
@@ -34,32 +36,46 @@ def get_cells(sheet,name):
     cells = worksheet.get_all_records(empty2zero=False, head=1, default_blank='')
     return cells
 
-from sqlalchemy import Table, Column, Integer, Unicode, MetaData, create_engine
+from sqlalchemy import Table, Column, Integer, Unicode, Date, Numeric, MetaData, create_engine
 from sqlalchemy.orm import mapper, create_session
 
 class Row(object):
     pass
 
-def store_sqlite(data,outfile, header_rows = 2, cols = None, types = None):
-  engine = create_engine('sqlite:///'+outfile)
+def store_sqlite(data,outfile, header_rows = 1, cols = None, types = None, indexes=None,tablename=None):
+    engine = create_engine('sqlite:///'+outfile)
     metadata = MetaData(bind=engine)
 
-    t = Table('rows', metadata,
-              Column('id', Integer, primary_key=True),
-              *(Column(c, Unicode(255)) for c in cols))
+    types_map = {
+        "text": Unicode(255),
+        "number": Numeric(),
+        "date":Date()
+    }
 
-    metadata.drop_all(engine)
-    metadata.create_all(engine)
-    mapper(Row, t)
     
-    session = create_session(bind=engine, autocommit=False, autoflush=True)
+    t =Table(tablename if tablename else 'rows', metadata,
+             Column('id', Integer, primary_key = True),
+             *(Column(c,
+                      types_map.get(types[c],Unicode(255))  if types else Unicode(255),
+                      index=True if indexes and indexes[c]==1 else False)
+               for c in cols))
 
+
+    try:
+        t.drop()
+    except sqlalchemy.exc.OperationalError as err:
+        print err
+        print "continuing"
+    t.create()
+    
+    mapper(Row, t)
+    session = create_session(bind=engine, autocommit=False, autoflush=True)
     for j,e in enumerate(data):
-        if j < header_rows: continue
+        if j < (header_rows - 1): continue
         
         r = Row()
         for i,c in enumerate(cols):
-            setattr(r,c,e[c])
+            setattr(r,c,dateparser.parse( e[c]) if types and types[c] == "date" else e[c] )
         session.add(r)
 
     session.commit()
@@ -69,31 +85,35 @@ if __name__=="__main__":
     import argparse
     parser = argparse.ArgumentParser()
 
-    
-    parser.add_argument('outfile', metavar='O', nargs='?', help='output file name')
-    
-    parser.add_argument('--sqlite', default=False, action="store_const", const=True)
-
-    parser.add_argument('--sheet', help='sheet key', default="1WWOD92D7SvCVh2Hlu-0dyyuIOctn7UMVMSOPoFBbGgI")
-    parser.add_argument('--name', help='sheet name', default="INFO")
-
-    parser.add_argument('--header-rows',help='n header rows', type=int, default = 2)
-    parser.add_argument('--cols-row',dest ="c_row", default =0, help="row containing column names", type=int)
-    parser.add_argument('--types-row',dest="t_row",default =1, help="row containing column types", type=int)
-    
-                 
-    
+    parser.add_argument('--output, -o', dest="output", required=True, help='output file')
+    parser.add_argument('--sqlite', default=True, action="store_const", const=True, help="create a sqlite database instead of a flat file")
+    parser.add_argument('--sheet, -s', dest="sheet",help='google spreadsheet workbook key', default="1WWOD92D7SvCVh2Hlu-0dyyuIOctn7UMVMSOPoFBbGgI")
+    parser.add_argument('--name, -n', dest="name", help='google speadsheet page name', default="DATABASE")
+    parser.add_argument('--header-rows',dest="h_rows", help='n header rows', type=int, default = 2)
+    parser.add_argument('--cols-row',dest ="c_row", default =0, help="[SQLITE3] row containing column names", type=int)
+    parser.add_argument('--types-row',dest="t_row",default=None, help="[SQLITE3] row containing column types", type=int)
+    parser.add_argument('--index-row',dest="i_row",default=None, help="[SQLITE3] row containing binary values for whether or not to create an sqlite index", type=int)
+    parser.add_argument('--table -t',dest="tablename",default ="rows", help="[SQLITE3] specify a table name for", type=str)
+ 
     args = parser.parse_args()
 
     if args.sqlite:
         cells =get_cells(args.sheet, args.name)
-        cols = cells[args.c_row]
-        types = cells[args.t_row]
-        
+        #cols = cells[args.c_row] if args.c_row != None else None
+        cols=cells[0].keys()
+        types = cells[args.t_row -2] if args.t_row else None
+        indexes = cells[args.i_row -2] if args.i_row else None
+
+        print types
+        print indexes
+ 
         store_sqlite(cells,
-                     args.outfile,
+                     args.output,
                      cols = cols,
-                     types = types)
+                     indexes = indexes,
+                     types = types,
+                     header_rows=args.h_rows,
+                     tablename = args.tablename)
         
     else:
         print_json(args.sheet, args.name, args.outfile)
